3cbcc440301e81d440a76b4257acf026fe729309
Lbov Dmitriy <dmitriylb@hflabs.ru>
2024-12-05 11:10:20 +0300

    first commit

---
+++ src/main/java/info/lbov/Inheritance.java
@@ -0,0 +1,4 @@
+package info.lbov;
+
+public class Inheritance {
+}
---
+++ src/main/java/info/lbov/Main.java
@@ -0,0 +1,7 @@
+package info.lbov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
