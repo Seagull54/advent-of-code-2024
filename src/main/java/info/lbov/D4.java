6cc53546c0e6c5a53bc47972f9a6f0591c0403ed
Lbov Dmitriy <dmitriylb@hflabs.ru>
2024-12-05 17:03:02 +0300

    days days 4-5

--- src/main/java/info/lbov/D4.java
+++ src/main/java/info/lbov/D4.java
@@ -3,7 +3,12 @@ package info.lbov;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class D4 {
 
@@ -17,6 +22,7 @@ public class D4 {
 
         int height = horizontal.size();
         int width = horizontal.getFirst().length();
+
 
         int sum = 0;
         for (int i = 1; i < width-1; i++) {
@@ -40,6 +46,73 @@ public class D4 {
     public static char getChar(List<String> lines, int x, int y) {
         return lines.get(y).charAt(x);
     }
+
+    public static void findXmasRegex(String filePath) throws IOException {
+        Pattern XMASpattern = Pattern.compile("XMAS");
+        Pattern SAMXpattern = Pattern.compile("SAMX");
+        List<String> horizontal = Files.readAllLines(Path.of(filePath));
+
+        List<String> vertical = IntStream.range(0, horizontal.getFirst().length())
+                .boxed()
+                .map(index -> horizontal
+                        .stream().map(line -> line.charAt(index))
+                        .map(String::valueOf)
+                        .collect(Collectors.joining())
+                ).toList();
+
+        int height = horizontal.size();
+        int width = horizontal.getFirst().length();
+
+        List<String> diagonal = Stream.concat(
+            IntStream.range(0, horizontal.getFirst().length())
+                .boxed()
+                .map(start -> {
+                    StringBuilder result = new StringBuilder();
+                    StringBuilder backwardsResult = new StringBuilder();
+                    int x = start;
+                    int y = 0;
+                    int yback = horizontal.size() - 1;
+                    int steps = width - start;
+                    for (int i = 0; i < steps; i++) {
+                        result.append(horizontal.get(y + i).charAt(x + i));
+
+                    }
+                    for (int i = 0; i < start+1; i++) {
+                        backwardsResult.append(horizontal.get(y + i).charAt(x - i));
+                    }
+
+                    return List.of(result.toString(), backwardsResult.toString());
+                }),
+            IntStream.range(1, horizontal.size())
+                .boxed()
+                .map(start -> {
+                    StringBuilder result = new StringBuilder();
+                    StringBuilder backwardsResult = new StringBuilder();
+                    int x = 0;
+                    int y = start;
+                    int xback = width - 1;
+                    int steps = height - start;
+                    for (int i = 0; i < steps; i++) {
+                        result.append(horizontal.get(y + i).charAt(x + i));
+                    }
+                    for (int i = 0; i < steps ; i++) {
+                        backwardsResult.append(horizontal.get(y + i).charAt(xback - i));
+                    }
+                    return List.of(result.toString(), backwardsResult.toString());
+                })
+            ).flatMap(Collection::stream).toList();
+
+
+        Long result = Stream.concat(
+            Stream.concat(horizontal.stream(), vertical.stream()),
+            diagonal.stream())
+            .map(
+                    line -> XMASpattern.matcher(line).results().count() + SAMXpattern.matcher(line).results().count()
+            ).reduce(Long::sum).orElse(0L);
+
+        System.out.println(result);
+    }
+
 
 
 }
